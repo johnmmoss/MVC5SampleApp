07570b87df8e839ba46d4898b8076b78ffad5f5c
johnmmoss <johnmmoss@gmail.com>
2015-04-10 22:03:16 +0100

    Add of first few ActionResult examples

---
+++ src/MVC5SampleApp/Controllers/SimpleController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+
+namespace MVC5SampleApp.Controllers
+{
+    public class SimpleController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public EmptyResult Empty()
+        {
+            // _emailSender.Send("subject", "Doing something async");
+
+            return new EmptyResult();
+        }
+
+        public ContentResult Content()
+        {
+            var content = new ContentResult();
+            content.Content = "<html><body bgcolor=\"red\"><p>This is my HTML content</body></html>";
+            content.ContentType = "text/html";
+            return content;
+
+        }
+
+        public ContentResult ContentXml()
+        {
+            var content = new ContentResult();
+            content.Content = "<books><book>one</book><book>two</book></books>";
+            content.ContentType = "text/xml";
+            return content;
+        }
+
+        public HttpUnauthorizedResult Unauthorized()
+        {
+            var httpUnauthorizedResult = new HttpUnauthorizedResult();
+
+            return httpUnauthorizedResult;
+        }
+    }
+}
