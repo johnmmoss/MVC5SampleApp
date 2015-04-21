9edf9281355bf905d19077ac4ef94e23089e6136
johnmmoss <johnmmoss@gmail.com>
2015-04-21 20:57:42 +0100

    Added a form to test CSRF

---
+++ src/MVC5SampleApp/Controllers/FeedbackController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using MVC5SampleApp.Models;
+
+namespace MVC5SampleApp.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(FeedbackModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Complete", "Feedback", new { email=model.Email });
+        }
+
+        public ActionResult Complete(string email)
+        {
+            return View((object)email);
+        }
+	}
+}
---
+++ src/MVC5SampleApp/Models/FeedbackModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC5SampleApp.Models
+{
+    public class FeedbackModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Surname { get; set; }
+        [Required]
+        [StringLength(600)]
+        public string Text { get; set; }
+    }
+}
