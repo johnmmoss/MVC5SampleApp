e049772b9da486e451a1b98ff95370a369b6c047
johnmmoss <johnmmoss@gmail.com>
2015-05-05 20:10:04 +0100

    Added in ASP.NET Identity packages, files and content

--- src/MVC5SampleApp/Global.asax.cs
+++ src/MVC5SampleApp/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MVC5SampleApp
@@ -13,6 +14,8 @@ namespace MVC5SampleApp
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
