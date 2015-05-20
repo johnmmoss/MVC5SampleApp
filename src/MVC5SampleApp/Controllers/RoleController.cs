31a187cc49520c22778f339c203ff8b244ad6452
johnmmoss <johnmmoss@gmail.com>
2015-05-20 18:45:30 +0100

    Updated menu and added new user, role and notice pages

---
+++ src/MVC5SampleApp/Controllers/RoleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5SampleApp.Controllers
+{
+    public class RoleController : Controller
+    {
+        //
+        // GET: /Role/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
