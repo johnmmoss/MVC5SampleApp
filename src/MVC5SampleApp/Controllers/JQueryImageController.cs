2f6a07865e6ef5beefbab82854ff6b009d719d8c
johnmmoss <johnmmoss@gmail.com>
2015-10-10 17:41:38 +0100

    Added a demo page for JQuery Image Cycle2 slideshow

---
+++ src/MVC5SampleApp/Controllers/JQueryImageController.cs
@@ -0,0 +1,57 @@
+﻿using System.Web.Mvc;
+
+namespace MVC5SampleApp.Controllers
+{
+    public class JQueryImageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /JQueryImage/
+        public ActionResult SlideShow()
+        {
+            return View();
+        }
+
+        public ActionResult Zoom()
+        {
+            return View();
+        }
+
+        public ActionResult Overlays()
+        {
+            return View();
+        }
+
+        public ActionResult LazyLoad()
+        {
+            return View();
+        }
+
+        public ActionResult Tooltips()
+        {
+            return View();
+        }
+
+        public ActionResult Carousel()
+        {
+            return View();
+            
+        }
+
+        public ActionResult Filter()
+        {
+            return View();
+            
+        }
+
+        public ActionResult RoundCorners()
+        {
+            return View();
+            
+        }
+	}
+}
