2297a5a774d202c58a26cc6ad516aa19fdf09244
johnmmoss <johnmmoss@gmail.com>
2015-10-12 21:48:29 +0100

    A first simple typeahead demo

---
+++ src/MVC5SampleApp/Controllers/TypeaheadController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5SampleApp.Controllers
+{
+    public class TypeaheadController : Controller
+    {
+        //
+        // GET: /Typeahead/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
