37a7bda1d9d43de0cdd54c0ccf154d779237dfb8
johnmmoss <johnmmoss@gmail.com>
2015-10-17 15:20:55 +0100

    Added in a simple api call typeahead example

--- src/MVC5SampleApp/App_Start/BundleConfig.cs
+++ src/MVC5SampleApp/App_Start/BundleConfig.cs
@@ -33,7 +33,7 @@ namespace MVC5SampleApp
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
--- src/MVC5SampleApp/Controllers/TypeaheadController.cs
+++ src/MVC5SampleApp/Controllers/TypeaheadController.cs
@@ -14,5 +14,70 @@ namespace MVC5SampleApp.Controllers
         {
             return View();
         }
-	}
+
+        public JsonResult States(string q)
+        {
+            var states = new[]
+                {
+                    "Alabama", 
+                    "Alaska", 
+                    "Arizona", 
+                    "Arkansas", 
+                    "California",
+                    "Colorado",
+                    "Connecticut",
+                    "Delaware",
+                    "Florida",
+                    "Georgia",
+                    "Hawaii",
+                    "Idaho",
+                    "Illinois",
+                    "Indiana",
+                    "Iowa",
+                    "Kansas",
+                    "Kentucky",
+                    "Louisiana",
+                    "Maine",
+                    "Maryland",
+                    "Massachusetts",
+                    "Michigan",
+                    "Minnesota",
+                    "Mississippi",
+                    "Missouri",
+                    "Montana",
+                    "Nebraska",
+                    "Nevada",
+                    "New Hampshire",
+                    "New Jersey",
+                    "New Mexico",
+                    "New York",
+                    "North Carolina",
+                    "North Dakota",
+                    "Ohio",
+                    "Oklahoma",
+                    "Oregon",
+                    "Pennsylvania",
+                    "Rhode Island",
+                    "South Carolina",
+                    "South Dakota",
+                    "Tennessee",
+                    "Texas",
+                    "Utah",
+                    "Vermont",
+                    "Virginia",
+                    "Washington",
+                    "West Virginia",
+                    "Wisconsin",
+                    "Wyoming"
+                };
+
+            if (!string.IsNullOrEmpty(q))
+            {
+                states = states.Where(
+                    x => x.ToLower().StartsWith(q.ToLower())).ToArray();
+            }
+            return Json(states, JsonRequestBehavior.AllowGet);
+
+        }
+    }
 }
